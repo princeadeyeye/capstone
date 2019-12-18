2480c6c83448f4dfbf15e350629d0392d5f4ff4c
Muiz Adeyeye <muizadeyeye@hmail.com>
2019-12-18 08:12:11 +0100

    Updates

---
+++ precache-manifest.9c24238b9e18229e4f61a4b3ee438bdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7aa6d8254900645cdfb10ab62a8423",
+    "url": "/capstone/index.html"
+  },
+  {
+    "revision": "d6bf4e7ebada5768bcad",
+    "url": "/capstone/static/css/main.b77c51c1.chunk.css"
+  },
+  {
+    "revision": "d0df0fcd7cfd5f6d1fd5",
+    "url": "/capstone/static/js/2.6673f177.chunk.js"
+  },
+  {
+    "revision": "d6bf4e7ebada5768bcad",
+    "url": "/capstone/static/js/main.00603266.chunk.js"
+  },
+  {
+    "revision": "1845a322f5e95fb00bec",
+    "url": "/capstone/static/js/runtime-main.f6021a8c.js"
+  },
+  {
+    "revision": "ab1615166235d9a38a79da4620f1b268",
+    "url": "/capstone/static/media/01.ab161516.jpg"
+  },
+  {
+    "revision": "e6c272bbdeafbc11589bdfb63d6bc1d6",
+    "url": "/capstone/static/media/banner1.e6c272bb.jpg"
+  },
+  {
+    "revision": "6d560509cd30501c423507862cfdada0",
+    "url": "/capstone/static/media/face.6d560509.jpg"
+  }
+]);
